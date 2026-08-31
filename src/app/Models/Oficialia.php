45fdb9189d0d6d38ab19cb5f406028837003c079
artemisacortes <artemisa.cortesjacobo@gmail.com>
2026-08-31 11:24:05 -0600

    Update(oficialia) Modelo actualizado.

--- src/app/Models/Oficialia.php
+++ src/app/Models/Oficialia.php
@@ -10,7 +10,7 @@ class Oficialia extends Model
     //use HasFactory;
     protected $table = 'oficialia';
     protected $primaryKey = 'id';
-    protected $fillable = ['user_id', 'oficio_id' ,'delegacion', 'tipo_tramite','oficio', 'area_turno','usuario_responsable','fecha','hora', 'estatus', 'ruta_oficio', 'conclusion' ,'created_at','updated_at'];   
+    protected $fillable = ['user_id', 'oficio_id' ,'delegacion', 'tipo_tramite','oficio', 'area_turno','precedencia','usuario_responsable','fecha','hora','fecha_registro', 'hora_registro', 'fecha_turno', 'hora_turno','termino','fecha_termino','hora_termino', 'estatus', 'ruta_oficio', 'conclusion' ,'created_at','updated_at'];   
 
     public function usuarioResponsable()
     {
