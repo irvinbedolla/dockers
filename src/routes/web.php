dc4428f7df4928888cef16461c6ad39323ad7271
artemisacortes <artemisa.cortesjacobo@gmail.com>
2026-09-01 09:09:01 -0600

    Fix(cumplimientos): Ruta en acceso a todos los usuarios.

--- src/routes/web.php
+++ src/routes/web.php
@@ -321,7 +321,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
             Route::delete('/audieniecias/pago_eliminar_pago/{id_solicitud}',    [SeerController::class, 'pago_eliminar_pago'])->name('pago_eliminar_pago');
             Route::post('/solicitudes/terminar_audiencia',                      [SeerController::class, 'terminar_audiencia'])->name('terminar_audiencia');
             Route::post('/audiencias/eliminar_item_sesion/{id}',                [SeerController::class, 'eliminar_item_sesion'])->name('eliminar_item_sesion');
-            Route::get('/audienicas/cumplimietos/{id}',                         [SeerController::class, 'ver_pagos_audiencia'])->name('audiencia_cumplimientos');
+            
             Route::get('/cumplimientos/detalle/{id}',                           [SeerController::class, 'ver_pago_cumplimiento'])->name('pago_cumplimiento');
             Route::post('/guardar_edicion_audiencia',                           [SeerController::class, 'audiencia_confirmar'])->name('audiencia_confirmar');
             Route::post('/audiencias/pagoA',                                    [SeerController::class, 'pagoA_audiencia'])->name('pagoA_audiencia'); // cumplimiento en audiencias
@@ -441,6 +441,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/solicitudes/home',                     [SeerController::class, 'solicitudes'])->name('solicitudes_index');
     Route::post('/seer/aserorias',                      [SeerController::class, 'store_asesorias'])->name('seer.store_asesoria');
     Route::get('/seer/index',                           [SeerController::class, 'index'])->name('seer');
+    Route::get('/audienicas/cumplimietos/{id}',                         [SeerController::class, 'ver_pagos_audiencia'])->name('audiencia_cumplimientos');
 
 
     //Solicitudes y casos de exepcion
