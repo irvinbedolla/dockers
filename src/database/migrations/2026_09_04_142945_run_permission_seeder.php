049708515874550ee989332e58bfe14d4a0dac3a
artemisacortes <artemisa.cortesjacobo@gmail.com>
2026-09-04 15:27:41 -0600

    New(migration): run_permission_seeder
    Migracion que ejecuta el archivo de seeder RolePermissionSeeder

---
+++ src/database/migrations/2026_09_04_142945_run_permission_seeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Artisan;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+
+        Artisan::call('db:seed', [
+            '--class' => 'RolePermissionSeeder',
+            '--force' => true
+        ]);
+    }
+
+    public function down(): void
+    {
+        // Opcional: podrías dejarlo vacío o implementar lógica de reversión
+    }
+};
