c2b7c30db399884921e9237bb64c5fe70b791c0f
Jorge Correa Gutiérrez <jorgecgjr@hotmail.com>
2026-09-09 16:08:38 -0600

    Fix: Notifications status
    Notification status has been fixed in view ver_citado_historial.blade and its POST method

--- src/app/Http/Controllers/SeerController.php
+++ src/app/Http/Controllers/SeerController.php
@@ -15552,13 +15552,15 @@ class SeerController extends Controller
             //'updated_at'               => $fecha_actualizar,
         ]);
 
-        if($data["estatus"] == "Sin asignar"){
+        $estatus = $data["estatus"] ?? $folio->estatus;
+
+        if($estatus == "Sin asignar"){
             $data_update = SeerCitados::find($data["id"])
-            ->update(['estatus' => $data["estatus"], 'id_notificador' => 0]);
+            ->update(['estatus' => $estatus, 'id_notificador' => 0]);
         }
         else{
             $data_update = SeerCitados::find($data["id"])
-            ->update(['estatus' => $data["estatus"]]);
+            ->update(['estatus' => $estatus]);
         }
         /*
         $fecha_inicio = $data["fecha_inicio"];
